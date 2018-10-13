5df0309656ef3adfe780de148348605be661d4b6
Walee Ahmed <dev@walee.me>
2018-10-13 11:49:23 -0400

    fixed routing

--- apps/web/src/containers/App.js
+++ apps/web/src/containers/App.js
@@ -1,33 +1,22 @@
 import React, { Component } from "react"
-import logo from "../logo.svg"
 import "./App.css"
 import { ApolloProvider } from "react-apollo"
-
+import Router from "react-router-dom/BrowserRouter"
 import initApollo from "../lib/initApollo"
+import Pages from "../pages"
 
 const client = initApollo()
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </ApolloProvider>
+      <div className="App">
+        <ApolloProvider client={client}>
+          <Router>
+            <Pages />
+          </Router>
+        </ApolloProvider>
+      </div>
     )
   }
 }
