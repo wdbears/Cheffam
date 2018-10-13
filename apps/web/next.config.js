ad0114fddd9db49fbd9a69d15333d10aa68c15a6
Walee Ahmed <dev@walee.me>
2018-10-13 04:49:51 -0400

    added next plugins to handle offline caching & env variables

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,10 @@
-const withTypescript = require('@zeit/next-typescript')
-const withTM = require('next-plugin-transpile-modules');
+const withTypescript = require("@zeit/next-typescript")
+const withTM = require("next-plugin-transpile-modules")
+const withPlugins = require("next-compose-plugins")
+const withOffline = require("next-offline")
+const nextEnv = require("next-env")
+const dotenvLoad = require("dotenv-load")
 
-module.exports = withTypescript(
-  withTM({
-    transpileModules: [
-      "@ramen_stack/test"
-    ]
-  })
-)
+dotenvLoad()
+
+module.exports = withPlugins([nextEnv(), withOffline(), withTypescript()])
