123ef03db04da1acefdd8fb947bd39f7aa3d543d
Walee Ahmed <dev@walee.me>
2018-10-14 03:21:53 -0400

    fixed sticky bottom navbar

--- apps/web/src/components/OrderBadges.js
+++ apps/web/src/components/OrderBadges.js
@@ -1,76 +1,80 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import SwipeableViews from 'react-swipeable-views';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import PastOrdersList from './PastOrdersList';
-import CurrentOrderList from './CurrentOrderList';
+import React from "react"
+import PropTypes from "prop-types"
+import { withStyles } from "@material-ui/core/styles"
+import SwipeableViews from "react-swipeable-views"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
+import Typography from "@material-ui/core/Typography"
+import PastOrdersList from "./PastOrdersList"
+import CurrentOrderList from "./CurrentOrderList"
 
 function TabContainer({ children, dir }) {
   return (
     <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
       {children}
     </Typography>
-  );
+  )
 }
 
 TabContainer.propTypes = {
   children: PropTypes.node.isRequired,
-  dir: PropTypes.string.isRequired,
-};
+  dir: PropTypes.string.isRequired
+}
 
 const styles = {
   root: {
-    flexGrow: 1,
-  },
-};
+    // flexGrow: 1,
+  }
+}
 
 class FullWidthTabs extends React.Component {
   state = {
-    value: 0,
-  };
+    value: 0
+  }
 
   handleChange = (event, value) => {
-    this.setState({ value });
-  };
+    this.setState({ value })
+  }
 
   handleChangeIndex = index => {
-    this.setState({ value: index });
-  };
+    this.setState({ value: index })
+  }
 
   render() {
-    const { classes, theme } = this.props;
+    const { classes, theme } = this.props
 
     return (
       <div className={classes.root}>
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            fullWidth
-          >
-            <Tab label="Current Order" />
-            <Tab label="Past Orders" />
-          </Tabs>
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          fullWidth
+        >
+          <Tab label="Current Order" />
+          <Tab label="Past Orders" />
+        </Tabs>
         <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
           index={this.state.value}
           onChangeIndex={this.handleChangeIndex}
         >
-          <TabContainer dir={theme.direction}><CurrentOrderList /></TabContainer>
-          <TabContainer dir={theme.direction}><PastOrdersList /></TabContainer>
+          <TabContainer dir={theme.direction}>
+            <CurrentOrderList />
+          </TabContainer>
+          <TabContainer dir={theme.direction}>
+            <PastOrdersList />
+          </TabContainer>
         </SwipeableViews>
       </div>
-    );
+    )
   }
 }
 
 FullWidthTabs.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
-};
+  theme: PropTypes.object.isRequired
+}
 
-export default withStyles(styles, { withTheme: true })(FullWidthTabs);
+export default withStyles(styles, { withTheme: true })(FullWidthTabs)
