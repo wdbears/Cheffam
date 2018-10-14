6ecee3e8ea51eb168cce57d3ebbfd1ad79a4d88f
Ahmad Raza <raza2157@gmail.com>
2018-10-14 02:10:59 -0400

    Removed excessive code

--- apps/web/src/components/ChefCards.js
+++ apps/web/src/components/ChefCards.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import { withStyles } from '@material-ui/core/styles';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from '@material-ui/core/CardContent'
-import ProfileName from "../components/ProfileName";
 import ImageAvatars from "../components/Avatar"
 
 const styles = {
@@ -36,7 +33,6 @@ function MediaCard({ classes, imgSrc}) {
         <CardMedia
           className={classes.media}
           image={imgSrc}
-          title="Contemplative Reptile"
         />
       </Card>
   );
