1a2a863d45a2832c371f8d8fb02b59f1d692ed43
Ahmad Raza <raza2157@gmail.com>
2018-10-14 09:28:43 -0400

    Updated profile card

--- apps/web/src/pages/chefProfile.js
+++ apps/web/src/pages/chefProfile.js
@@ -1,19 +1,35 @@
 import React from "react"
 import ImageAvatars from "../components/Avatar"
 import { withStyles } from "@material-ui/core/styles"
-import MediaCard from "../components/FoodCategoryTile"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardMedia from "@material-ui/core/CardMedia"
+import Typography from "@material-ui/core/Typography"
+
 const styles = {
 avatars: {paddingTop: 200},
-itemWrapper: {  display: 'flex',
+itemWrapper: {
     justifyContent: 'center',
     alignItems: 'center'
    }
 }
 
-const ChefProfile = ({classes}) => <div>
+const ChefProfile = ({classes, imgSrc, textSrc}) => <div>
     <ImageAvatars className = "classes.avatars" />
     <div className={classes.itemWrapper}>
-    <MediaCard />
+    <Card className={classes.card}>
+      <CardActionArea
+        style={{ width: "100%", backgroundColor: "rgba(0,0,0,0)" }}
+      >
+        <CardMedia
+          className={classes.media}
+          style={{ display: "flex", justifyContent: "center" }}
+          image={imgSrc}
+        >
+          <Typography className={classes.cardTitle}>{textSrc}</Typography>
+        </CardMedia>
+      </CardActionArea>
+    </Card>
     </div>
 </div>
 
