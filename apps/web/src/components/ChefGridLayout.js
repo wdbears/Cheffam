9318a67752f6936b8a9ff00a3c68c47233fdecd3
Walee Ahmed <dev@walee.me>
2018-10-14 05:03:31 -0400

    added graphql data loading to homepage

--- apps/web/src/components/ChefGridLayout.js
+++ apps/web/src/components/ChefGridLayout.js
@@ -1,33 +1,52 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
 import ChefCards from "./ChefCards"
-import AmericanFood from "../images/homemadesteak.jpg"
-import Mac from "../images/americanhomemade.jpg"
 import MalaysianFood from "../images/malaysian.jpg"
 import IndianFood from "../images/indian.jpg"
-import KoreanFood from "../images/korean.jpg"
-import ChineseFood from "../images/chinese.jpg"
-import JapaneseFood from "../images/japanese.jpg"
-import VietFood from "../images/vietnamese.jpg"
+// import KoreanFood from "../images/korean.jpg"
+// import ChineseFood from "../images/chinese.jpg"
+// import JapaneseFood from "../images/japanese.jpg"
+// import VietFood from "../images/vietnamese.jpg"
+// import AmericanFood from "../images/homemadesteak.jpg"
+// import Mac from "../images/americanhomemade.jpg"
+
+import gql from "graphql-tag"
+import { Query } from "react-apollo"
+
+const GET_CHEFS = gql`
+  {
+    allUsers {
+      id
+      firstName
+      lastName
+      chefRating
+    }
+  }
+`
 
 function CenteredGrid(props) {
   return (
     <div>
       <Grid container spacing={16} alignContent="center">
-        {[
-          { img: AmericanFood, text: "Chef Bob" },
-          { img: MalaysianFood, text: "Chef Ahmad" },
-          { img: VietFood, text: "Chef Bob" },
-          { img: KoreanFood, text: "Chef Bob" },
-          { img: ChineseFood, text: "Chef Bob" },
-          { img: Mac, text: "Brandon" },
-          { img: JapaneseFood, text: "Jackie" },
-          { img: IndianFood, text: "Indian" }
-        ].map(({ img, text }, i) => (
-          <Grid item key={i} xs={12} sm={6} md={4} lg={3}>
-            <ChefCards imgSrc={img} textSrc={text} />
-          </Grid>
-        ))}
+        {/*
+        TODO: Add modular prop data display in ChefCards
+         */}
+        <Query query={GET_CHEFS}>
+          {({ loading, error, data: { allUsers } }) =>
+            loading ? (
+              <h1>loading</h1>
+            ) : (
+              allUsers.map(({ id, firstName, lastName, chefRating }, i) => (
+                <Grid item key={i} xs={12} sm={6} md={4} lg={3}>
+                  <ChefCards
+                    imgSrc={i % 3 === 0 ? IndianFood : MalaysianFood}
+                    textSrc={`${firstName} ${lastName}`}
+                  />
+                </Grid>
+              ))
+            )
+          }
+        </Query>
       </Grid>
     </div>
   )
