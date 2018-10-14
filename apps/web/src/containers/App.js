38819b87838fa758844cd74838c63c2bc56bc693
Walee Ahmed <dev@walee.me>
2018-10-14 01:36:32 -0400

    added basic routing to pages

--- apps/web/src/containers/App.js
+++ apps/web/src/containers/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react"
 import { ApolloProvider } from "react-apollo"
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles"
 import Router from "react-router-dom/BrowserRouter"
 import initApollo from "../lib/initApollo"
 import Pages from "../pages"
-import CssBaseline from '@material-ui/core/CssBaseline';
-import BottomNav from '../components/BottomNav';
+import CssBaseline from "@material-ui/core/CssBaseline"
+import BottomNav from "../components/BottomNav"
 
 const client = initApollo()
 
 const styles = {
-  app: {display: 'flex', minHeight: '100vh', flexDirection: 'column'},
+  app: { display: "flex", minHeight: "100vh", flexDirection: "column" },
   navbar: { flex: 1 }
 }
 
@@ -18,23 +18,28 @@ class App extends Component {
   render() {
     return (
       <div className={this.props.classes.app}>
-        <header></header>
-        {console.log(this.props.classes)}
-        <main className={this.props.classes.navbar}>
-        <ApolloProvider client={client}>
-          <Router className="wrapper">
-              <>  {/* React fragment used for storing multiple components */}
-              <CssBaseline />
-              <Pages />
-              </>
-            </Router>
-        </ApolloProvider></main>
-        <footer>
-              <BottomNav className={this.props.classes.navbar}/>
+        <Router>
+          <>
+            <header />
+            {console.log(this.props.classes)}
+            <main className={this.props.classes.navbar}>
+              <ApolloProvider client={client}>
+                <>
+                  {" "}
+                  {/* React fragment used for storing multiple components */}
+                  <CssBaseline />
+                  <Pages />
+                </>
+              </ApolloProvider>
+            </main>
+            <footer>
+              <BottomNav className={this.props.classes.navbar} />
             </footer>
+          </>
+        </Router>
       </div>
     )
   }
 }
 
-export default withStyles(styles)(App);
+export default withStyles(styles)(App)
