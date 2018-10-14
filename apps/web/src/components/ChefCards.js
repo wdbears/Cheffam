70b1feb6c5e2971df78517f93f15ee785883ceb9
Ahmad Raza <raza2157@gmail.com>
2018-10-13 21:38:51 -0400

    Fixed merge conflict

--- apps/web/src/components/ChefCards.js
+++ apps/web/src/components/ChefCards.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
-<<<<<<< HEAD
-=======
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
->>>>>>> 3863df216ad030260e7c97947771ff1374c8e1ef
+import withStyles from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import Profile from '../components/Profile';
 import Halal from '../images/halal.jpg';
 
