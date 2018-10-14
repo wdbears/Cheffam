b4b27c3d5268a8da86575fc09b68299690542ba8
Walee Ahmed <dev@walee.me>
2018-10-13 20:33:21 -0400

    centered text in card

--- apps/web/src/components/FoodCategoryTile.js
+++ apps/web/src/components/FoodCategoryTile.js
@@ -1,52 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react"
+import PropTypes from "prop-types"
+import { withStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
 import AmericanFood from "../images/american.jpg"
 
 const styles = {
-  cardContainer: {
-    textAlign:'center'
-  },
   card: {
-    maxWidth: 345,
+    maxWidth: 345
   },
   media: {
-    width: 400,
-    height: 400
+    height: 140
   }
-};
+}
 
-function MediaCard(props) {
-  const { classes } = props;
+function MediaCard({ classes }) {
   return (
-    <div className="cardContainer">
     <Card className={classes.card}>
-      <CardActionArea>
+      <CardActionArea style={{ width: "100%" }}>
         <CardMedia
           className={classes.media}
+          style={{ display: "flex", justifyContent: "center" }}
           image={AmericanFood}
-          title="American Food"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            American
-          </Typography>
-        </CardContent>
+          title="Contemplative Reptile"
+        >
+          <Typography style={{ alignSelf: "center" }}>Yoooo</Typography>
+        </CardMedia>
       </CardActionArea>
     </Card>
-    </div>
-  );
+  )
 }
 
 MediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(MediaCard);
+export default withStyles(styles)(MediaCard)
